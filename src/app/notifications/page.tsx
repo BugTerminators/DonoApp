d1b73002944196f588c293fe6b423ede33169ec9
seshadri-dutta <seshadridutta@gmail.com>
2025-02-22 14:49:56 +0530

    Create a notification page

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import NotificationPage from '@/components/NotificationPage'
+import { Button } from '@/components/ui/button'
+import { ArrowLeft } from 'lucide-react'
 
-const page = () => {
+const Home = () => {
     return (
-        <div>Notifications...</div>
-    )
+    <>
+                <div className="px-4 py-2 md:py-6 md:px-8 flex items-center mb-6">
+                <Button variant="ghost" size="icon" className="mr-4">
+                    <ArrowLeft className="h-6 w-6" />
+                </Button>
+                <h1 className="text-2xl md:text-3xl font-semibold">Notifications</h1>
+            </div>
+    
+    <NotificationPage />
+    </>
+    );
 }
 
-export default page
+export default Home
