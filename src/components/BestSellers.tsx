e7ab19e7682c78caff971cf39a034b5f291864f4
imdebamrita <imdebamrita@gmail.com>
2025-02-23 01:34:42 +0530

    get all listings api connected

---
+++ src/components/BestSellers.tsx
@@ -0,0 +1,29 @@
+"use client";
+import BestSellerCard from './BestSellerCard';
+import React, { useEffect, useState } from 'react';
+import { Listing } from '@/backend/src/services/types';
+import { fetchListings } from '@/services/apis/listings';
+
+const BestSellers = () => {
+    const [listings, setListings] = useState<Listing[]>([]);
+
+    useEffect(() => {
+        const loadData = async () => {
+            const fetchedListings = await fetchListings();
+            setListings(fetchedListings);
+        };
+        loadData();
+    }, []);
+    return (
+        <div className="px-4 md:px-8">
+            <h2 className="text-2xl md:text-3xl font-bold mb-4">Best Seller</h2>
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-6">
+                {listings.map((product) => (
+                    <BestSellerCard key={product.id} product={product} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default BestSellers
